433dc5dfa8384cbad175da7036f236176add50f4
Lasse Jenssen <lasse@jcon.no>
2018-07-18 12:24:20 +0100

    Project: FirstApp

---
+++ FirstApp/src/main/java/no/eritec/demo/FirstApp/App.java
@@ -0,0 +1,13 @@
+package no.eritec.demo.FirstApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
