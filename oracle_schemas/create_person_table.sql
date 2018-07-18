eafd726e1d81bc2366d867ff75426ecac2154e0d
Lasse Jenssen <lasse@jcon.no>
2018-07-19 00:36:41 +0100

    Changes in oracle_schemas

---
+++ oracle_schemas/create_person_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person (id number, first_name varchar2(50));
+
+INSERT INTO person VALUES (1, 'SCOTT');
+INSERT INTO person VALUES (2, 'VALERIE');
+INSERT INTO person VALUES (3, 'JIM');
+
+COMMIT;
